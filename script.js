6bf03513569b962cb0a16a3c8e93b39cff287ce9
Cipri19 <149061628+Cipri19@users.noreply.github.com>
2023-12-08 20:07:33 +0200

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 var i = 0;          
 var images = [];   
-var time = 3000;    
+var time = 3000;  
+var button = document.createElement("buton");
+
 
 images[0] = "C:\Users\cimpe\OneDrive\Desktop\Informatica Aplicata\resursecpu.png";
 images[1] = "C:\Users\cimpe\OneDrive\Desktop\Informatica Aplicata\resursedisk.png";
 images[2] = "C:\Users\cimpe\OneDrive\Desktop\Informatica Aplicata\resursememorie.png";
-
-function changeImg(){
+button.addEventListener("click", function changeImg(){
     document.slide.src = images[i];
 
     if(i < images.length - 1){
